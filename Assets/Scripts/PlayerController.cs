863039a2a09700fa9ce5dc9737ae95de52eaba3f
hunganh7204 <tranhunganh2004@gmail.com>
2025-10-14 23:08:27 +0700

    add BG and Player

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+using UnityEditor.Build.Content;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float flapForce = 5f;
+    public AudioClip flapSFX;
+    private Rigidbody rb;
+    private bool isAlive = true;
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        audioSource = gameObject.AddComponent<AudioSource>();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isAlive) return;
+
+        if(Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space){
+            Flap();
+        }
+
+        float angle = Mathf.Clamp(rb.angularVelocity.y*5f, -90f, 45f);
+        transform.rotation = Quaternion.Euler(0,0,angle);
+    }
+
+    void Flap()
+    {
+        rb.velocity = Vector2.up * flapForce;
+        if (flapSFX != null) audioSource.PlayOneShot(flapSFX);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        isAlive = false;
+        GameManager.Instance.PlayerDied();
+    }
+
+    public void ResetPlayer(Vector3 startpos)
+    {
+        transform.position = startpos;
+        rb.velocity = Vector2.zero;
+        transform.rotation = Quaternion.identity;
+        isAlive = true;
+    }
+}
